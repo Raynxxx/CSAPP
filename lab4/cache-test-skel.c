3a4a802c2387a3c7d3880cbbd2fef832f49beb43
Raynxxx <rayn1027@outlook.com>
2015-08-13 20:02:51 +0800

    begin lab4

---
+++ lab4/cache-test-skel.c
@@ -0,0 +1,78 @@
+/*
+YOUR NAME HERE
+CSE 351 - Autumn 2014
+Lab 4 - Mystery Caches
+
+Mystery Cache Geometries:
+mystery0:
+    block size =
+    cache size =
+    associativity =
+mystery1:
+    block size =
+    cache size =
+    associativity =
+mystery2:
+    block size =
+    cache size =
+    associativity =
+mystery3:
+    block size =
+    cache size =
+    associativity =
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mystery-cache.h"
+
+/*
+ * NOTE: When using access_cache() you do not need to provide a "real" memory
+ * addresses. You can use any convenient integer value as a memory address,
+ * you should not be able to cause a segmentation fault by providing a memory
+ * address out of your programs address space as the argument to access_cache.
+ */
+
+/*
+   Returns the size (in B) of each block in the cache.
+*/
+int get_block_size(void) {
+  /* YOUR CODE GOES HERE */
+
+  return -1;
+}
+
+/*
+   Returns the size (in B) of the cache.
+*/
+int get_cache_size(int block_size) {
+  /* YOUR CODE GOES HERE */
+  return -1;
+}
+
+/*
+   Returns the associativity of the cache.
+*/
+int get_cache_assoc(int cache_size) {
+  /* YOUR CODE GOES HERE */
+  return -1;
+}
+
+int main(void) {
+  int size;
+  int assoc;
+  int block_size;
+
+  cache_init(0,0);
+
+  block_size=get_block_size();
+  size=get_cache_size(block_size);
+  assoc=get_cache_assoc(size);
+
+  printf("Cache block size: %d bytes\n", block_size);
+  printf("Cache size: %d bytes\n", size);
+  printf("Cache associativity: %d\n", assoc);
+
+  return EXIT_SUCCESS;
+}
