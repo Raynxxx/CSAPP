cbe1f137d98557130528e08fdaed0d7a45eb488e
Raynxxx <rayn1027@outlook.com>
2015-08-13 12:46:54 +0800

    get to lab3 level 2

---
+++ lab3/bang.d
@@ -0,0 +1,13 @@
+
+bang.o：     文件格式 elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 b8 5e 19 b5 4d c9 	movabs $0x608d84c94db5195e,%rax
+   7:	84 8d 60 
+   a:	48 89 04 25 08 23 60 	mov    %rax,0x602308
+  11:	00 
+  12:	68 20 10 40 00       	pushq  $0x401020
+  17:	c3                   	retq   
---
+++ lab3/bang.s
@@ -0,0 +1,4 @@
+mov $0x608d84c94db5195e, %rax
+mov %rax, 0x602308
+pushq $0x401020
+retq
